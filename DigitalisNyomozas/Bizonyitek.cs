90ef012eac09b4458b0a4ab8c41b599000f9d620
VViktor <Viktorka2030@gmail.com>
2026-01-30 09:04:48 +0100

    Bizonyitek osztaly letrehozva

---
+++ DigitalisNyomozas/Bizonyitek.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class Bizonyitek
+	{
+		private string azonosito;
+		private string tipus;
+		private string leiras;
+		private int megbizhatosag;
+
+		public Bizonyitek(string azonosito, string tipus, string leiras, int megbizhatosag)
+		{
+			this.azonosito = azonosito;
+			this.tipus = tipus;
+			this.leiras = leiras;
+			this.megbizhatosag = megbizhatosag;
+		}
+
+		public string Azonosito { get => azonosito; set => azonosito = value; }
+		public string Tipus { get => tipus; set => tipus = value; }
+		public string Leiras { get => leiras; set => leiras = value; }
+		public int Megbizhatosag { get => megbizhatosag; set => megbizhatosag = value; }
+	}
+}
