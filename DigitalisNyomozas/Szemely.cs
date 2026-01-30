764e73df09bb4ae78aae337237b3ba0f16592746
VViktor <Viktorka2030@gmail.com>
2026-01-30 08:58:31 +0100

    Szemely osztaly letrehozva

---
+++ DigitalisNyomozas/Szemely.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class Szemely
+	{
+		private string nev;
+		private int eletkor;
+		private string megjegyzes;
+
+		public Szemely(string nev, int eletkor, string megjegyzes)
+		{
+			this.nev = nev;
+			this.eletkor = eletkor;
+			this.megjegyzes = megjegyzes;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Eletkor { get => eletkor; set => eletkor = value; }
+		public string Megjegyzes { get => megjegyzes; set => megjegyzes = value; }
+	}
+}
