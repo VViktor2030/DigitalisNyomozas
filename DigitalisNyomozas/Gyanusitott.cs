302f26720beef9ec74ecadc68d913d854de4dbd7
VViktor <Viktorka2030@gmail.com>
2026-01-30 09:00:39 +0100

    Gyanusitott osztaly letrehozva

---
+++ DigitalisNyomozas/Gyanusitott.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class Gyanusitott
+	{
+		private Szemely szemely;
+		private int gyanusitottsag;
+		private string statusz;
+
+		public Gyanusitott(Szemely szemely, int gyanusitottsag, string statusz)
+		{
+			this.szemely = szemely;
+			this.gyanusitottsag = gyanusitottsag;
+			this.statusz = statusz;
+		}
+
+		public int Gyanusitottsag { get => gyanusitottsag; set => gyanusitottsag = value; }
+		public string Statusz { get => statusz; set => statusz = value; }
+		internal Szemely Szemely { get => szemely; set => szemely = value; }
+	}
+}
