3a9812262308b4b0583da653ea93478c10bc9014
VViktor <Viktorka2030@gmail.com>
2026-01-30 09:15:57 +0100

    Idovonal esemeny osztaly letrehozva

---
+++ DigitalisNyomozas/IdovonalEsemeny.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class IdovonalEsemeny
+	{
+		private DateOnly datum;
+		private string leiras;
+
+		public IdovonalEsemeny(DateOnly datum, string leiras)
+		{
+			this.datum = datum;
+			this.leiras = leiras;
+		}
+
+		public DateOnly Datum { get => datum; set => datum = value; }
+		public string Leiras { get => leiras; set => leiras = value; }
+	}
+}
